70c3993389df63900f41cf5aef06c4e080110064
benriquez <benjamin_enriquez@ligph.com>
2021-02-26 10:50:07 +0800

    Update files

--- src/index.js
+++ src/index.js
@@ -1,55 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index';
 import * as serviceWorker from './serviceWorker';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
-} from "@apollo/client";
 
-
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
-  cache: new InMemoryCache()
-});
-
-// const client = new ApolloClient({
-//   uri: 'http://localhost:4000/',
-//   cache: new InMemoryCache(),
-//   headers: {
-//     authorization: localStorage.getItem('token'),
-//     'client-name': 'WidgetX Ecom [web]',
-//     'client-version': '1.0.0'
-//   }
-// });
-
-client
-  .query({
-    query: gql`
-      query GetPost {
-        post(id:14) {
-          id
-          title
-          content
-          comments {
-            content
-          }
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
+import { Provider } from 'react-redux';
+import store from './redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </React.StrictMode>,
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/pages/Pages.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Switch, Route, useLocation } from 'react-router-dom';
+
+import useAuth from '../hooks/useAuth';
+
+import Posts from '../components/Posts';
+import SinglePage from './SinglePage';
+import SingleEditPage from './SingleEditPage';
+import SingleNewPage from './SingleNewPage';
+
+const Pages = () => {
+  const {isLoggedIn} = useAuth();
+  const location = useLocation();
+  const path = location.pathname;
+
+  return (
+    <main className={`app-main${path === '/' ? '' : ' app-main-single'}`}>
+      <Switch>
+        {isLoggedIn &&
+        <Route path={`/news/new`}>
+          <SingleNewPage />
+        </Route>}
+        {isLoggedIn &&
+        <Route path={`/news/edit/:id`}>
+          <SingleEditPage />
+        </Route>}
+        <Route path={`/news/:id`}>
+          <SinglePage />
+        </Route>
+        <Route path='/' exact>
+          <Posts />
+        </Route>
+      </Switch>
+    </main>
+  );
+}
+
+export default Pages;
